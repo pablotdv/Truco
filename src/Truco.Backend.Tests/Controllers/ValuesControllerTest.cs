2e0c7c2ffa5b7abee5e04095868ca39c9aa255f7
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2018-01-10 23:03:32 -0200

    Create the unit tests to ValuesController

---
+++ src/Truco.Backend.Tests/Controllers/ValuesControllerTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Truco.Backend.Controllers;
+using Xunit;
+using System.Linq;
+
+namespace Truco.Backend.Tests.Controllers
+{
+    public class ValuesControllerTest
+    {        
+        [Fact]
+        public void Get_ShouldReturnListOfStrings()
+        {
+            // Arrange
+            var controller = new ValuesController();
+
+            // Act
+            var result = controller.Get().ToList();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Count);
+            Assert.Equal("value1", result[0]);
+            Assert.Equal("value2", result[1]);
+        }
+
+        [Fact]
+        public void Get_ShouldReturnString()
+        {
+            // Arrange
+            var controller = new ValuesController();
+
+            // Act
+            var result = controller.Get(1);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal("value1", result);
+        }
+        
+    }
+}
