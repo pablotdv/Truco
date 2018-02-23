9fed10ae6c41d3228ea89a679eb82142113cacc0
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2018-02-22 23:29:28 -0300

    Testes unitários de Models.Pais

---
+++ src/Truco.Backend/Data/TrucoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Truco.Backend.Models;
+
+namespace Truco.Backend.Data
+{
+    public class TrucoDbContext : DbContext
+    {
+        public DbSet<Pais> Paises { get; set; }
+    }
+}
---
+++ src/Truco.Backend/Models/Pais.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+using Truco.Backend.Models.Resources;
+
+namespace Truco.Backend.Models
+{
+    [Table("Paises")]
+    [DisplayColumn("Nome")]
+    public class Pais
+    {
+        [Key]
+        public Guid PaisId { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(PadraoResource), ErrorMessageResourceName = nameof(PadraoResource.Required))]
+        [StringLength(200, ErrorMessageResourceType = typeof(PadraoResource), ErrorMessageResourceName = nameof(PadraoResource.StringLength))]
+        [Display(Name = "País")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(PadraoResource), ErrorMessageResourceName = nameof(PadraoResource.Required))]
+        [StringLength(3, ErrorMessageResourceType = typeof(PadraoResource), ErrorMessageResourceName = nameof(PadraoResource.StringLength))]
+        [Display(Name = "Sigla")]
+        public string Sigla { get; set; }
+    }
+}
