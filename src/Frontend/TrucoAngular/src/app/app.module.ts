d3a1c00ce99f17d16bb6ea0fdee06c5cc359dca2
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2018-02-28 22:30:05 -0300

    Frontend angular

--- src/Frontend/TrucoAngular/src/app/app.module.ts
+++ src/Frontend/TrucoAngular/src/app/app.module.ts
@@ -1,32 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { PaisService } from './pais.service';
-import { IndexComponent } from './components/index/index.component';
-import { CreateComponent } from './components/create/create.component';
-import { EditComponent } from './components/edit/edit.component';
-import { appRoutes } from './routerConfig';
-import { CoinService } from './coin.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    IndexComponent,
-    CreateComponent,
-    EditComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot(appRoutes),
-    HttpClientModule
+    BrowserModule
   ],
-  providers: [PaisService,
-    CoinService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
